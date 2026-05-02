51882458681f132580e29b572d7c5545ae33d854
Sabinam Mahato <sksingh4919@gmail.com>
2026-05-02 21:38:27 +0545

    put on the /

--- Sabinam/src/pages/projects.jsx
+++ Sabinam/src/pages/projects.jsx
@@ -31,7 +31,7 @@ const Projects = () => {
   const [oprojects, setoProjects] = useState([]);
 
   useEffect(() => {
-    fetch("https://backend-nine-beta-78.vercel.app/api/featured")   
+    fetch("https://backend-nine-beta-78.vercel.app/api/featured/")   
       .then((res) => res.json())
       .then((data) => {
         console.log("Projects from backend:", data); 
@@ -41,7 +41,7 @@ const Projects = () => {
   }, []);
 
     useEffect(() => {
-    fetch("https://backend-nine-beta-78.vercel.app/api/other-projects")   
+    fetch("https://backend-nine-beta-78.vercel.app/api/other-projects/")   
       .then((res) => res.json())
       .then((data) => {
         console.log("Projects from backend:", data); 
--- backend/contacts/urls.py
+++ backend/contacts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ContactMessageCreateView,ContactMessageView,ContactDeleteView,home
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('/', home, name='home'),
     path('contact/', ContactMessageCreateView.as_view(), name='contact-create'),
     path('message/', ContactMessageView.as_view(), name='message'),
     path('messages/<int:pk>/delete/', ContactDeleteView.as_view(), name='message-delete'),
